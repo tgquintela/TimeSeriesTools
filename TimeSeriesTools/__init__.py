198e5572b4db9ad0337a77719051a510629914f7
antonio <tgq.spm@gmail.com>
2016-12-02 00:26:12 +0100

    Adding test of transformations.

--- TimeSeriesTools/__init__.py
+++ TimeSeriesTools/__init__.py
@@ -35,8 +35,8 @@ def test():
     ## Tests of modules
     test_artificial_data.test()
 #    test_utils.test()
-    test_measures.test()
-#    test_transformations.test()
+#    test_measures.test()
+    test_transformations.test()
 #    test_burstdetection.test()
 #    test_tsstatistics.test()
 #    test_regimedetection.test()
