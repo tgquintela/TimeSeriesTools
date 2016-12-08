7f1be4ded3c0914fc9bc7a09f4305fb62a758c9b
antonio <tgq.spm@gmail.com>
2016-12-08 10:11:07 +0100

    Exploring tests.

--- TimeSeriesTools/__init__.py
+++ TimeSeriesTools/__init__.py
@@ -34,8 +34,8 @@ warnings.simplefilter("ignore")
 def test():
     ## Tests of modules
     test_artificial_data.test()
-    test_utils.test()
-##    test_measures.test()
+#    test_utils.test()
+    test_measures.test()
 #    test_transformations.test()
 #    test_burstdetection.test()
 #    test_tsstatistics.test()
