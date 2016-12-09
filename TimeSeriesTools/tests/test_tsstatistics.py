ad05118c95e94f3d67bd98151703d8673af782da
antonio <tgq.spm@gmail.com>
2016-12-10 00:41:06 +0100

    Exploring tests: ts statistics.

--- TimeSeriesTools/tests/test_tsstatistics.py
+++ TimeSeriesTools/tests/test_tsstatistics.py
@@ -160,8 +160,8 @@ def test():
                      logscale=False)
     isi_distribution(spks, 10, globally=False, normalized=False,
                      logscale=False)
-#    isi_distribution(spks, 10, globally=False, normalized=True,
-#                     logscale=True)
+    isi_distribution(spks, 10, globally=False, normalized=True,
+                     logscale=True)
 #    isi_distribution(spks, 10, globally=True, normalized=False,
 #                     logscale=True)
 
