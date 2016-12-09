19804e7a47108bb44f12bb6920b4b2e5dce75b69
antonio <tgq.spm@gmail.com>
2016-12-09 23:08:26 +0100

    Exploring tests: ts statistics.

--- TimeSeriesTools/tests/test_tsstatistics.py
+++ TimeSeriesTools/tests/test_tsstatistics.py
@@ -153,16 +153,16 @@ def test():
 #    ## Interevent statistics
 #    ##
     isis_computation(spks, logscale=False)
-    isis_computation(spks, logscale=True)
+#    isis_computation(spks, logscale=True)
 #
-    isi_distribution(spks, 10, globally=False, normalized=True,
-                     logscale=True)
-    isi_distribution(spks, 10, globally=True, normalized=True,
-                     logscale=False)
-    isi_distribution(spks, 10, globally=False, normalized=False,
-                     logscale=False)
-    isi_distribution(spks, 10, globally=True, normalized=False,
-                     logscale=True)
+#    isi_distribution(spks, 10, globally=False, normalized=True,
+#                     logscale=True)
+#    isi_distribution(spks, 10, globally=True, normalized=True,
+#                     logscale=False)
+#    isi_distribution(spks, 10, globally=False, normalized=False,
+#                     logscale=False)
+#    isi_distribution(spks, 10, globally=True, normalized=False,
+#                     logscale=True)
 
     temporal_si(spks)
 
