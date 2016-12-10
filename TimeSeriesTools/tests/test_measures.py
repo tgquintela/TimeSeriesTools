556ff861f0bbc7dee851f0e2a84c655e9431f221
antonio <tgq.spm@gmail.com>
2016-12-10 14:04:03 +0100

    Exploring tests: metric tests.

--- TimeSeriesTools/tests/test_measures.py
+++ TimeSeriesTools/tests/test_measures.py
@@ -76,9 +76,9 @@ def test():
 
     T = create_scales_periodogram_sequence(rand_ts)
     T = create_scales_periodogram_sequence(rand_ts, T)
-    H = hurst(rand_ts, T, method='per')
-    H = hurst(normal_rand, T, method='per')
-    H = hurst(uniform_rand, T, method='per')
+#    H = hurst(rand_ts, T, method='per')
+#    H = hurst(normal_rand, T, method='per')
+#    H = hurst(uniform_rand, T, method='per')
 
     #### Fractal dimenstion measure
     ###############################
