a50c2a3f3168106e0bfe33a16a803b7e6eddd94d
antonio <tgq.spm@gmail.com>
2016-12-10 12:00:26 +0100

    Exploring tests: test measures.

--- TimeSeriesTools/tests/test_measures.py
+++ TimeSeriesTools/tests/test_measures.py
@@ -41,23 +41,6 @@ def test():
     mea = entropy(raster)
     assert(type(mea) == float)
 
-#    ## Hurst measure
-#    T = create_RS_scales_sequence(vals, sequence='complete')
-#    R_S, T = hurst_alternative_rs_values(vals, T)
-#    mea = general_rs_fit(R_S, T)
-#    print '0', mea
-#    R_S, T = hurst_rs_values(vals, T)
-#    mea = general_rs_fit(R_S, T)
-#    print '1', mea
-#    T = create_RS_scales_sequence(vals, sequence='power')
-#    R_S, T = hurst_rs_values(vals, T)
-#    mea = general_rs_fit(R_S, T)
-#    print '2', mea
-#    T = create_RS_scales_sequence(vals, sequence=T)
-#    R_S, T = hurst_rs_values(vals, T)
-#    mea = general_rs_fit(R_S, T)
-#    print '3', mea
-
     #### Hurst measure
     ##################
     ## Some failure. Not correct values obtained
@@ -93,9 +76,9 @@ def test():
 
     T = create_scales_periodogram_sequence(rand_ts)
     T = create_scales_periodogram_sequence(rand_ts, T)
-    H = hurst(rand_ts, T, method='per')
-    H = hurst(normal_rand, T, method='per')
-    H = hurst(uniform_rand, T, method='per')
+#    H = hurst(rand_ts, T, method='per')
+#    H = hurst(normal_rand, T, method='per')
+#    H = hurst(uniform_rand, T, method='per')
 
     #### Fractal dimenstion measure
     ###############################
