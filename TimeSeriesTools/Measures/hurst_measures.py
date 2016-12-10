982461347b74b9f1ec9c02222046ed8a9ee7339b
antonio <tgq.spm@gmail.com>
2016-12-10 14:40:26 +0100

    Exploring tests: metric tests.

--- TimeSeriesTools/Measures/hurst_measures.py
+++ TimeSeriesTools/Measures/hurst_measures.py
@@ -231,11 +231,7 @@ def hurst_per(X, M=None, fit_method='loglogLSQ'):
     ## Compute values of length for that scales
     L, M = hurst_per_values(X, M)
     ## Fit of the function
-    assert(len(L) == len(M))
-    assert(np.all(np.isfinite(M)))
-    assert(np.all(np.isfinite(L)))
     measure = general_multiscale_fit(L, M, fit_method)
-#    measure = 0.5
     ## Computation of the Hurst parameter
     H = per_hurst_measure(measure)
     return H
